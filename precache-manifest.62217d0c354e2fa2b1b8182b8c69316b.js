8d9f582ab74c33c6088ca482c75415641f03b70b
Apollo21552 <41828188+Apollo21552@users.noreply.github.com>
2019-01-21 20:21:29 -0500

    Updates

---
+++ precache-manifest.62217d0c354e2fa2b1b8182b8c69316b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "1446d6ec194445c3572d",
+    "url": "/robofriends/static/js/runtime~main.1446d6ec.js"
+  },
+  {
+    "revision": "12a286fdd79d1722978d",
+    "url": "/robofriends/static/js/main.12a286fd.chunk.js"
+  },
+  {
+    "revision": "1f6e374b610e4852c6bc",
+    "url": "/robofriends/static/js/1.1f6e374b.chunk.js"
+  },
+  {
+    "revision": "12a286fdd79d1722978d",
+    "url": "/robofriends/static/css/main.666dc31c.chunk.css"
+  },
+  {
+    "revision": "1f6e374b610e4852c6bc",
+    "url": "/robofriends/static/css/1.cbaa1261.chunk.css"
+  },
+  {
+    "revision": "488c06614c511cb71b707a3306cc4210",
+    "url": "/robofriends/index.html"
+  }
+];
